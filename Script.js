aa6b712da85e17b471f46e53bdec42789bf575b5
Najeh <mn.najeh1314@gmail.com>
2024-01-11 09:21:45 +0100

    First commit

---
+++ Script.js
@@ -0,0 +1,11 @@
+// Exemple de fonctions JavaScript pour ajouter des produits au panier et passer une commande
+
+function addToCart() {
+    // Logique pour ajouter un produit au panier
+    // Vous devrez implémenter cette fonction en fonction de vos besoins
+}
+
+function checkout() {
+    // Logique pour passer une commande
+    // Vous devrez implémenter cette fonction en fonction de vos besoins
+}
