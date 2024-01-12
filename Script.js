b6be55d099652896bf7d165f7e3ca68a08a18734
Najeh <mn.najeh1314@gmail.com>
2024-01-12 18:10:56 +0100

    updates to continue

--- Script.js
+++ Script.js
@@ -1,6 +1,10 @@
 const products = [
-    { name: "T-shirt", price: 20, image: "tshirt.jpg" },
-    { name: "Jeans", price: 40, image: "jeans.jpg" },
+    { name: "T-shirt", price: 20, image: "image1.jpeg" },
+    { name: "Jeans", price: 40, image: "image2.jpeg" },
+    { name: "Robe", price: 35, image: "image3.jpeg" },
+    { name: "Chaussures", price: 50, image: "image4.jpeg" },
+    { name: "Casquette", price: 15, image: "image5.jpeg" },
+    { name: "Sac à dos", price: 30, image: "image6.jpeg" },
     // Ajoutez d'autres produits ici
 ];
 
@@ -29,6 +33,13 @@ function displayProducts() {
 
         // Ajouter un événement de clic pour sélectionner le produit
         productDiv.addEventListener("click", () => selectProduct(product));
+
+        // Remplir la liste déroulante dans la page "achat.html"
+        const productList = document.getElementById("product-list");
+        const option = document.createElement("option");
+        option.value = product.name;
+        option.textContent = product.name;
+        productList.appendChild(option);
     });
 }
 
