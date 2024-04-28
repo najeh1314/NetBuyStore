6557789132716370cd7bafec264890eeba140104
Najeh <mn.najeh1314@gmail.com>
2024-04-28 12:42:44 +0100

    graphQL API

--- RestfulWSMI/src/main/java/com/pi/CitoyenRepository.java
+++
@@ -1,8 +0,0 @@
-package com.pi;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-//CECI EST LE REPRESNTANT DU MODEL DE LA TABLE CITOYEN DANS LA BD
-public interface CitoyenRepository extends JpaRepository<Citoyen, Integer>{
-
-}
